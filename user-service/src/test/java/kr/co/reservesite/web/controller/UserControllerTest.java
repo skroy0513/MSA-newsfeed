eb17f5cf51aa3120008ee959a722c7a4a4b86b40
skroy0513 <skroy0513@gmail.com>
2024-03-30 03:43:16 +0900

    회원가입, 로그인 테스트 코드

--- user-service/src/test/java/kr/co/reservesite/web/controller/UserControllerTest.java
+++ user-service/src/test/java/kr/co/reservesite/web/controller/UserControllerTest.java
@@ -1,15 +1,22 @@
 package kr.co.reservesite.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,18 +24,36 @@ class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @DisplayName("테스트 코드 테스트")
     void home() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders.get("/user/"))
+        mockMvc.perform(get("/user/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello world"));
     }
 
     @Test
-    void register() {
+    @DisplayName("회원가입 테스트")
+    void register() throws Exception {
+        //given
+        Map<String, String> input = new HashMap<>();
+        input.put("name", "kim");
+        input.put("password", "1234");
+        input.put("email", "kim@gmail.com");
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/user/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(input)))
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 
     @Test
@@ -36,7 +61,23 @@ class UserControllerTest {
     }
 
     @Test
-    void login() {
+    @DisplayName("로그인 테스트")
+    void login() throws Exception {
+        //given
+        Map<String, String> input = new HashMap<>();
+        input.put("email", "kim@gmail.com");
+        input.put("password", "1234");
+
+        //when
+        ResultActions resultActions = mockMvc.perform(post("/user/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(input)))
+                .andDo(print());
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
     }
 
     @Test
